6a686e7f77c7f7fd50b4b0e1553732cdf7216dff
Adriana Bauer <33072028+adribauertech@users.noreply.github.com>
2023-10-14 10:31:44 -0300

    Add files via upload

---
+++ menu_site.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+    return render_template('homepage.html')
+
+@app.route("/investimentos")
+def investimentos():
+    return render_template('investimentos.html')
+
+@app.route("/usuarios/<nome_usuario>")
+def usuarios(nome_usuario):
+    return render_template('usuarios.html', nome_usuario=nome_usuario)
+
+if __name__ == "__main__":
+    app.run(debug= True)
+
